073de869c943dea1f0eac3550d503129404ef4eb
Amilcar-Steban <amilcarshm@gmail.com>
2024-02-12 18:37:53 -0500

    com2

--- pay-app-spring-microservices/app-invoice/src/main/java/com/aforo/model/Invoice.java
+++ pay-app-spring-microservices/app-invoice/src/main/java/com/aforo/model/Invoice.java
@@ -13,22 +13,6 @@ public class Invoice implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     private Integer idInvoice;
-    private double amount;
+    private Double amount ;
     private Integer state ;
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public int getState() {
-        return state;
-    }
-
-    public void setState(int state) {
-        this.state = state;
-    }
 }
